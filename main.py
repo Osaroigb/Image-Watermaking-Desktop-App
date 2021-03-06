3c747a0efe56658cb1371047da8da53ac92b14eb
Osaro <emmyigbinovia@gmail.com>
2021-03-06 15:09:34 +0100

    Resizing image to fit GUI window

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageTk, ImageDraw, ImageFont
 
 # create the main window of an application
 window = Tk()
-window.minsize(width=320, height=20)
+window.minsize(width=600, height=600)
 window.config(padx=40, pady=20)
 window.title("Image Watermarking App")
 
@@ -16,6 +16,12 @@ def upload_image():
     # open an image and create an image object from it
     img = Image.open(path)
 
+    # getting initial image dimensions
+    width, height = img.size
+
+    # (width, height) resize the uploaded image to fit the tkinter window
+    img = img.resize((500, 500), Image.ANTIALIAS)
+
     # create a draw object
     draw = ImageDraw.Draw(img)
 
@@ -23,18 +29,15 @@ def upload_image():
     logo = "OSAROIGB"
     img_font = ImageFont.truetype(font="arial.ttf", size=36)
 
-    # position the text by calculating the x and y coordinates
+    # position the text in the top right corner by calculating the x and y coordinates
     text_width, text_height = draw.textsize(text=logo, font=img_font)
-    width, height = img.size
+    resize_width, resize_height = img.size
 
-    x = width - text_width - 10
-    y = height / 3
+    x = resize_width - text_width - 10
+    y = 0
 
     # draw watermark on image via draw object
     draw.text(xy=(x, y), text=logo, fill="purple", font=img_font)
-
-    # save the watermarked image in the working directory
-    img.save("watermark.png")
 
     # create a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object
     tk_image = ImageTk.PhotoImage(img)
@@ -44,9 +47,15 @@ def upload_image():
     img_label.image = tk_image
     img_label.pack()
 
+    # resizing the image back to it's original dimensions
+    img = img.resize((width, height), Image.ANTIALIAS)
+
+    # save the watermarked image in the working directory
+    img.save("watermarked.jpeg")
+
 
 img_button = Button(text="Open an image", command=upload_image)
-img_button.pack()
+img_button.pack(pady=20)
 
 # start the GUI
 window.mainloop()
